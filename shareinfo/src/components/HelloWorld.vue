ab1d3470f99d809166b4e4c5b53d4a973ce67a38
changxianglin <281532612@qq.com>
2018-06-24 15:23:48 +0800

    some about router use

---
+++ shareinfo/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <div>{{this.msg}}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleGetData () {
+      axios.get('https://www.easy-mock.com/mock/5aead18d44f4206ef1c42cfd/example/test', {
+          params: {
+            id: this.$route.params.id
+          }
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+  mounted () {
+    console.log('to get data')
+    this.handleGetData()
+  },
+  watch: {
+      '$route' (to, from) {
+        console.log('change routes')
+        this.handleGetData()
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
