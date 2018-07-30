2f2c4fbd75c722df4787961b32a90986b4b03cc6
changxianglin <281532612@qq.com>
2018-07-30 23:33:26 +0800

    base route

--- imoocmall/src/router/index.js
+++ imoocmall/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import GoodsList from './../views/GoodsList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods/:goodsId/user/:name',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
