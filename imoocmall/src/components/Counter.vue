2928fb119d0e2688c0709f24b099cbcb9a6ef591
changxianglin <281532612@qq.com>
2018-07-30 23:19:15 +0800

    base props

---
+++ imoocmall/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click = "increment">+</button>
+    <button @click = "decrement">-</button>
+    <p><span>{{number}}</span></p>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: ['number'],
+        name: "Counter.vue",
+        data () {
+          return {
+            number: 0
+          }
+        },
+      methods: {
+          increment () {
+            this.$emit('incre')
+          },
+          decrement () {
+            this.$emit('decre')
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
