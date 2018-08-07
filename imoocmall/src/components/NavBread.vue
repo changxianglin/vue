68741b349834c7cd0f5e2ca0b9ebe4980859e758
changxianglin <281532612@qq.com>
2018-08-07 23:05:13 +0800

    Goods header footer bread

---
+++ imoocmall/src/components/NavBread.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "nav-bread.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- imoocmall/src/views/GoodsList.vue
+++ imoocmall/src/views/GoodsList.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+      <nav-header></nav-header>
+      <nav-bread></nav-bread>
       这是商品列表页面
       <span>{{$route.params.goodsId}}</span><br>
       <span>{{$route.params.name}}</span>
@@ -13,13 +15,21 @@
 </template>
 
 <script>
+  import './../assets/css/base.css'
+  import './../assets/css/product.css'
+  import NavHeader from '@/components/NavHeader'
+  import NavFooter from '@/components/NavFooter'
+  import NavBread from '@/components/NavBread'
     export default {
-
       methods: {
           jump () {
             this.$router.push({path: '/cart?goodsId=321'})
           }
-      }
+      },
+      components:｛
+            NavHeader,
+            NavFooter,
+            NavBread,
     }
 </script>
 
