aa696a163abeee45579e8a6a4b0676adc2373955
changxianglin <281532612@qq.com>
2018-08-13 22:33:50 +0800

    logout

--- imoocmall/server/routes/users.js
+++ imoocmall/server/routes/users.js
@@ -41,4 +41,17 @@ router.post('/login', (req, res, next) => {
 })
 })
 
+// 登出接口
+router.post("/logout", (req, res, next) => {
+  res.cookie('userId', '', {
+    path: '/',
+    maxAge: -1
+})
+res.json({
+  status: '0',
+  msg: '',
+  result: ''
+})
+})
+
 module.exports = router;
