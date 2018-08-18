25f4a86be6ae35e48cf7fc503b9493221be57936
changxianglin <281532612@qq.com>
2018-08-18 23:58:30 +0800

    editAll

--- imoocmall/server/routes/users.js
+++ imoocmall/server/routes/users.js
@@ -147,4 +147,39 @@ router.post('/cartEdit', (req, res, next) => {
   }
 })
 })
+
+router.post("/editCheckAll", (req, res, next) =>｛
+  var userId = req.cookies.userId
+  var checkAll = req.body.checkAll ? '1': '0'
+  User.findOne({userId: userId}, (err, user) => {
+    if(err) {
+      res.json({
+        status: '1',
+        msg: err.message,
+        result: ''
+      })
+    } else {
+      if(user) {
+        user.cartList.forEach((item)  => {
+          item.checked = checkAll
+        })
+        user.save(function (err1. doc) {
+          if(err1) {
+            res.json({
+              status: '1',
+              msg: err1.message,
+              result: ''
+            })
+          } else {
+            res.json({
+              status: '0',
+              msg: '',
+              result: 'suc'
+            })
+          }
+        })
+      }
+    }
+  })
+｝)
 module.exports = router;
